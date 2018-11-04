9d235351def50f0a954f5e983c1a35b606e5907d
Dheressa <aaDes.Dheressa@dbhds.virginia.gov>
2018-11-04 11:52:42 -0500

    test work_sanchez

--- Scripting_DB_Objects.ps1
+++ Scripting_DB_Objects.ps1
@@ -5,7 +5,7 @@ Get-PSDrive
 import-module SQLPS -DisableNameChecking;
 
 # script out all foreign keys in T-SQL
-dir SQLSERVER:\SQL\CSHSTESTDB3_1\DEFAULT\databases\Dental\tables | % {$_.foreignkeys } | % {$_.script()}; 
+dir SQLSERVER:\SQL\CVTC14\DEFAULT\databases\Azure_ID\tables | % {$_.foreignkeys } | % {$_.script()}; 
 
 # save the script to a file by adding out-file at the end of the code
 dir SQLSERVER:\SQL\CSHSTESTDB3_1\DEFAULT\databases\Dental\tables | % {$_.foreignkeys } | % {$_.script()} | out-file F:\DatabaseMaintenance\fk.sql -force;
